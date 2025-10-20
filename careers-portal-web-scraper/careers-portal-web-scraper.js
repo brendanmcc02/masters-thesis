c6dafee8d960108f85dd2573ae5ab8b7a2bcc6cd
brendanmcc02 <brendan.mccann02@gmail.com>
2025-10-20 11:53:37 +0100

    why is this shit so fucking hard bro

--- careers-portal-web-scraper/careers-portal-web-scraper.js
+++ careers-portal-web-scraper/careers-portal-web-scraper.js
@@ -1,7 +1,6 @@
 const puppeteer = require('puppeteer');
-const cheerio = require('cheerio');
+// const cheerio = require('cheerio');
 const fs = require('fs');
-const { title } = require('process');
 
 async function main() {
     const courses = await getCourses();
@@ -11,35 +10,89 @@ async function main() {
 
 async function getCourses() {
     const url = 'https://careersportal.ie/courses/coursefinder?types_in=2';
-    let courses = {};
+    let courses = [];
 
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto(url, { waitUntil: 'networkidle0' });
+    await page.goto(url, { waitUntil: 'networkidle0' }); // networkidle0 may be faulty apparently
 
     while (true) {
         let courseCards = await page.$$("div.group\\/card");
+        
+        // DL847
+        // CAO
+        // Level
+        // 8
+        // 4 Years
+        // QQI Links
+        // 605
+        // #
 
+        // let targetElements = await courseCard.$$("div.grid > div > span");
+        // console.log(targetElements.length);
+        // const openButton = await courseCard.$('label');
+        // if (openButton) {
+        //     console.log("we here!")
+        //     await openButton.click();
+        // }
+
+        // use waitForSelector!!!
         for (const courseCard of courseCards) {
-            const idElement = await courseCard.$("span.text-slate-500.font-bold");
+            const idElement = await courseCard.$("span.text-slate-500.font-bold"); 
             const id = idElement 
                 ? await idElement.evaluate(el => el.textContent.trim()) 
                 : "";
 
-            const typeElement = await courseCard.$("span.text-slate-300.text-\\[10px\\]");
+            // TODO doesn't work
+            const typeElement = await courseCard.$("div.grid > div:nth-child(2) > span:nth-child(2)");
             const type = typeElement 
                 ? await typeElement.evaluate(el => el.textContent.trim()) 
                 : "";
 
-
-            const titleElement = await courseCard.$("a.font-display.font-bold.text-skin-fill-primary.leading-tight.my-1");
+            const titleElement = await courseCard.$(".font-display.font-bold.text-skin-fill-primary.leading-tight.my-1");
             const title = titleElement 
                 ? await titleElement.evaluate(el => el.textContent.trim()) 
                 : "";
 
-            if (id) {
-                courses[id] = { id: id, type: type, title: title };
-            }
+            const collegeElement = await courseCard.$(".text-sm.leading-tight");
+            const college = collegeElement 
+                ? await collegeElement.evaluate(el => el.textContent.trim()) 
+                : "";
+
+            const durationElement = await courseCard.$("div.grid > div:nth-child(2) > span:nth-child(2)");
+            const duration = durationElement 
+                ? await durationElement.evaluate(el => el.textContent.trim()) 
+                : "";
+
+            const pointsElement = await courseCard.$("div.text-sm span.font-bold");
+            const points = pointsElement 
+                ? await pointsElement.evaluate(el => el.textContent.trim()) 
+                : "";
+
+            // TODO isAdditionalPortfolioTestInterviewRequired
+            const pointsContainer = await courseCard.$('div.col-span-1.flex.flex-col.items-center.justify-center.font-display div');
+            const pointsText = pointsContainer 
+                ? await pointsContainer.evaluate(el => el.textContent) 
+                : "";
+            // Note: The original HTML shows "#" with surrounding spaces/text: <span class=""> # </span>
+            const isAdditionalPortfolioTestInterviewRequired = pointsText.includes('#');
+
+            // TODO nfqLevel
+            const nfqLevelElement = await courseCard.$("div.grid > div:nth-child(7) > span.sr-only:last-child");
+            const nfqLevel = nfqLevelElement 
+                ? await nfqLevelElement.evaluate(el => el.textContent.trim()) 
+                : "";
+
+            courses.push({ 
+                id: id, 
+                type: type, 
+                title: title, 
+                college: college, 
+                duration: duration, 
+                points: points,
+                isAdditionalPortfolioTestInterviewRequired: isAdditionalPortfolioTestInterviewRequired,
+                nfqLevel: nfqLevel 
+            });
         }
         
         if (isNextButtonDisabled()) {
