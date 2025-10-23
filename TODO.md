593309b8bce6c7328101463176863c9bf211ecb5
brendanmcc02 <brendan.mccann02@gmail.com>
2025-10-23 13:23:04 +0100

    working pretty well

--- TODO.md
+++ TODO.md
@@ -2,7 +2,6 @@
 
 - [ ] finish careers portal web scraping
 - [ ] some courses have null points because they are not on careers portal?
-- [ ]
 - [ ] college major preprocessing on open-psychometrics dataset (**double check this work is valid before committing the time**)
 
 
@@ -11,4 +10,7 @@
 ### Title
 * some colleges have campus locations in their name
     * could we trim this? or remove courses that are similar to cut down on volume?
-* 
+* points
+    * what do we do about empty ones?
+        * could just fill it in manually tbh
+    * what about AQA? set to 0?
--- careers-portal-web-scraper/careers-portal-web-scraper.js
+++ careers-portal-web-scraper/careers-portal-web-scraper.js
@@ -14,7 +14,7 @@ async function getCourses() {
     const url = 'https://careersportal.ie/courses/coursefinder?types_in=2';
     let courses = [];
 
-    const browser = await puppeteer.launch({ headless: false, defaultViewport: null, args: ['--start-maximized', '--window-size=1920,1080', '--no-sandbox', '--disable-setuid-sandbox'] });
+    const browser = await puppeteer.launch({ headless: true, defaultViewport: null, args: ['--start-maximized', '--window-size=1920,1080', '--no-sandbox', '--disable-setuid-sandbox'] });
     const page = await browser.newPage();
     await page.setViewport({width: 1920, height: 1080});
     await page.goto(url, { waitUntil: 'networkidle0' });
@@ -22,14 +22,15 @@ async function getCourses() {
     while (true) {
         let courseHandles = await page.$$("div.group\\/card");
 
+        // for (let i = 17; i < courseHandles.length; i++) {
         for (const courseHandle of courseHandles) {
             const id = await getHandleTextFromSelector(courseHandle, "span.text-slate-500.font-bold");
             const type = await getHandleTextFromSelector(courseHandle, "span.text-slate-300.text-\\[10px\\]");
             const title = await getHandleTextFromSelector(courseHandle, "a.font-display.font-bold.text-skin-fill-primary.leading-tight.my-1.hover\\:text-skin-fill-secondary.hover\\:underline");
-            const college = await getHandleTextFromSelector(courseHandle, "a.text-sm.leading-tight.hover\\:text-skin-fill-secondary.hover\\:underline");
+            const college = await getHandleTextFromSelector(courseHandle, "div.col-span-2.flex.flex-col.items-start.justify-center.font-display > a.text-sm.leading-tight");
             const duration = await getHandleTextFromSelector(courseHandle, "span.text-sm.leading-tight");
-            const nfqLevel = await getHandleTextFromSelector(courseHandle, "div > span.sr-only:last-child");
-            const points = parseInt(await getHandleTextFromSelector(courseHandle, "div.text-sm > span.font-bold"));
+            const nfqLevel = parseInt(await getHandleTextFromSelector(courseHandle, "div > span.sr-only:last-child"));
+            const points = await getPoints(courseHandle);
 
             const additionalPortfolioTestInterviewRequiredText = await getHandleTextFromSelector(courseHandle, "div.text-sm > span:nth-child(2)");
             const isAdditionalPortfolioTestInterviewRequired = additionalPortfolioTestInterviewRequiredText.includes('#');
@@ -40,29 +41,31 @@ async function getCourses() {
             const expandButton = await courseHandle.$("div.flex.flex-col.items-center.justify-center.doNotPrint.font-display > label");
             await expandButton.click();
 
-            const overviewParagraphElementSelector = "div.prose.max-w-none.prose-sm.prose-slate.prose-headings\\:font-display.prose-headings\\:font-bold > div:nth-of-type(1) p";
+            const overviewElementSelector = "div.prose.max-w-none.prose-sm.prose-slate.prose-headings\\:font-display.prose-headings\\:font-bold > div:nth-of-type(1)";
             
             try {
-                await courseHandle.waitForSelector(overviewParagraphElementSelector, { visible: true, timeout: 15000 });
+                await courseHandle.waitForSelector(overviewElementSelector, { visible: true, timeout: 10000 });
             } catch (TimeoutError) {
-                console.log("Timeout waiting for " + overviewParagraphElementSelector + " on " + id);
+                courses.push({ 
+                    id: id,
+                    type: type,
+                    title: title,
+                    college: college,
+                    duration: duration,
+                    nfqLevel: nfqLevel,
+                    points: points,
+                    isAdditionalPortfolioTestInterviewRequired: isAdditionalPortfolioTestInterviewRequired,
+                    overview: "",
+                    careerOpportunities: "",
+                    interests: []
+                });
+                console.log("Timeout waiting for " + overviewElementSelector + " on " + id + ". Pushed incomplete course to dataset.");
                 continue;
             }
             
-            const overviewParagraphHandles = await courseHandle.$$(overviewParagraphElementSelector);
-            let overview = "";
-            for (overviewHandle of overviewParagraphHandles) {
-                // TODO, if <br>, replace with \n or space char?
-                overview += await overviewHandle.evaluate(el => el.textContent.trim()) + " ";
-            }
+            const overview = await getHandleTextFromSelector(courseHandle, overviewElementSelector);
 
-            const careerOpportunitiesParagraphElementSelector = "div.prose.max-w-none.prose-sm.prose-slate.prose-headings\\:font-display.prose-headings\\:font-bold > div:nth-of-type(2) p";
-            const careerOpportunitiesParagraphHandles = await courseHandle.$$(careerOpportunitiesParagraphElementSelector);
-            let careerOpportunities = "";
-            for (careerOpportunitiesHandle of careerOpportunitiesParagraphHandles) {
-                // TODO, if <br>, replace with \n or space char?
-                careerOpportunities += await careerOpportunitiesHandle.evaluate(el => el.textContent.trim()) + " ";
-            }
+            const careerOpportunities = await getHandleTextFromSelector(courseHandle, "div.prose.max-w-none.prose-sm.prose-slate.prose-headings\\:font-display.prose-headings\\:font-bold > div:nth-of-type(2)");
 
             let interests = []
             const interestsHandles = await courseHandle.$$("div.mt-8.flex.flex-wrap.items-center.gap-2 span");
@@ -85,13 +88,18 @@ async function getCourses() {
                 interests: interests
             });
 
+            // // todo temp
+            // await browser.close();
+            // return courses;
+            // //
+
             await sleepForMs(10000);
         }
         
         if (isNextButtonDisabled()) {
             // TODO
             // click next button
-            // refresh html or something
+            // wait for html to refresh
             fs.writeFileSync("../datasets/careers-portal/careers-portal-courses.json", JSON.stringify(courses, null, 4));
         } else {
             await browser.close();
@@ -116,6 +124,25 @@ function isNextButtonDisabled() {
 
 async function sleepForMs(ms) {
     await new Promise(r => setTimeout(r, ms));
+}
+
+async function getPoints(courseHandle) {
+    const points = await getHandleTextFromSelector(courseHandle, "div.col-span-1.flex.flex-col.items-center.justify-center.font-display > div.text-sm > span");
+
+    if (points === "AQA") {
+        // TODO what is the best thing to return here?
+        return 0; // or return "AQA" string? but that would mean different return types, poor design
+    } else if (points !== null) {
+        return parseInt(points);
+    }
+
+    const isNewCourse = await getHandleTextFromSelector(courseHandle, "div.text-xs.bg-skin-fill-secondary.rounded-full.px-2.text-white.font-bold");
+    if (isNewCourse === "New!") {
+        // TODO what is the best thing to return here?
+        return 0;
+    }
+
+    return null;
 }
 
 main();
