edc75750b0476bcf380944a20a70d0b48574b3ac
brendanmcc02 <brendan.mccann02@gmail.com>
2025-11-17 18:05:55 +0000

    friend data

--- datasets/open-psychometrics/filter_data/sbox.py
+++ datasets/open-psychometrics/filter_data/sbox.py
@@ -1,2 +1,18 @@
-import random as rd
-print(str(rd.randint(0,1)))
+def isPrimeNumber(i):
+    return False
+
+for i in range(100, 200): # [100, 101, ..., 199]
+    print(str(i))
+    if isPrimeNumber(i):
+        break
+
+
+
+# 1
+# 2
+# 3
+# 4
+# 5
+
+
+
