16b8d7eaffa41efa51936d88419d34f5049b585d
brendanmcc02 <brendan.mccann02@gmail.com>
2025-12-14 19:13:56 +0000

    removing other models from testing; just log reg

--- model-prototypes/traditional-prototype/testing_different_ml_models.py
+++ model-prototypes/traditional-prototype/testing_different_ml_models.py
@@ -2,15 +2,11 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
-from sklearn.naive_bayes import MultinomialNB, CategoricalNB
 from sklearn.metrics import top_k_accuracy_score
 from sklearn.dummy import DummyClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.neural_network import MLPClassifier
-from sklearn.svm import SVC
-from sklearn.ensemble import HistGradientBoostingClassifier, RandomForestClassifier
 
-number_of_top_k = 5
+number_of_top_k = 3
 
 dataset = pd.read_csv("../../datasets/open-psychometrics/clean_riasec_college_major_categories.tsv", sep='\t')
 
@@ -33,19 +29,6 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, stratify=y ,random_state=42
 )
 
-# trained models
-
-## multinomial naive bayes
-# multinomial_naive_bayes_model = MultinomialNB()
-# multinomial_naive_bayes_model.fit(X_train, y_train)
-# y_predicted_class_probabilities_multinomial_naive_bayes = multinomial_naive_bayes_model.predict_proba(X_test)
-
-## categorical naive bayes
-# categorical_naive_bayes_model = CategoricalNB()
-# categorical_naive_bayes_model.fit(X_train, y_train)
-# y_predicted_class_probabilities_categorical_naive_bayes = categorical_naive_bayes_model.predict_proba(X_test)
-
-## logistic regression
 logistic_regression_model = LogisticRegression(
     multi_class='multinomial',
     solver='saga', # negligible performance differences, saga is the quickest
@@ -56,50 +39,6 @@ logistic_regression_model = LogisticRegression(
 )
 logistic_regression_model.fit(X_train, y_train)
 y_predicted_class_probabilities_logistic_regression = logistic_regression_model.predict_proba(X_test)
-
-## multi-layer perceptron
-# (48, 20, 15) architecture: 48 inputs -> 2 hidden layers of 50 neurons each -> 15 outputs
-# alpha is the L2 penalty, similar to regularization
-# mlp_model = MLPClassifier(
-#     hidden_layer_sizes=(50, 50), 
-#     max_iter=500, # Often needs many iterations
-#     alpha=0.0001,
-#     warm_start=True,
-#     random_state=42
-# )
-# mlp_model.fit(X_train, y_train)
-# y_predicted_class_probabilities_mlp = mlp_model.predict_proba(X_test)
-# print("MLP Test Accuracy:                        " + str(round(top_k_accuracy_score(y_test, y_predicted_class_probabilities_mlp, k=number_of_top_k), 3)))
-
-## support vector machine (svm)
-# svm_model = SVC(
-#     kernel='linear',
-#     probability=True,
-#     C=1.0,
-#     random_state=42)
-# Note: SVMs scale poorly with N, so using a LinearSVC or linear kernel is often necessary for 77k rows.
-# For SVC, it's highly recommended to scale your features (0-4) before training.
-# svm_model = SVC(kernel='rbf', C=1.0, random_state=42) # slower
-# svm_model.fit(X_train, y_train)
-# y_predicted_class_probabilities_svm = svm_model.predict_proba(X_test)
-# print("SVM Test Accuracy:                        " + str(round(top_k_accuracy_score(y_test, y_predicted_class_probabilities_svm, k=number_of_top_k), 3)))
-
-## gradient boosting machines (gbm)
-# hgbm_model = HistGradientBoostingClassifier(random_state=42)
-# hgbm_model.fit(X_train, y_train)
-# y_predicted_class_probabilities_hgbm = hgbm_model.predict_proba(X_test)
-# print("Gradient Boosting Machines Test Accuracy: " + str(round(top_k_accuracy_score(y_test, y_predicted_class_probabilities_hgbm, k=number_of_top_k), 3)))
-
-# random forest
-# n_estimators is the number of trees; often start with 100 or 200
-# random_forest_model = RandomForestClassifier(
-#     n_estimators=200, 
-#     warm_start=True,
-#     random_state=42, 
-#     n_jobs=-1)
-# random_forest_model.fit(X_train, y_train)
-# y_predicted_class_probabilities_random_forest = random_forest_model.predict_proba(X_test)
-# print("Random Forest Test Accuracy:              " + str(round(top_k_accuracy_score(y_test, y_predicted_class_probabilities_random_forest, k=number_of_top_k), 3)))
 
 # baselines
 
@@ -116,16 +55,7 @@ most_frequent_model.fit(X_train, y_train)
 y_predicted_class_probabilities_most_frequent = most_frequent_model.predict_proba(X_test)
 
 # evaluation
-
-
-
-# print("\n# ACTUAL MODELS")
-# print("Multinomial Naive Bayes Test Accuracy:    " + str(round(top_k_accuracy_score(y_test, y_predicted_class_probabilities_multinomial_naive_bayes, k=number_of_top_k), 3)))
-# print("Categorical Naive Bayes Test Accuracy:    " + str(round(top_k_accuracy_score(y_test, y_predicted_class_probabilities_categorical_naive_bayes, k=number_of_top_k), 3)))
 print("Logistic Regression Test Accuracy:        " + str(round(top_k_accuracy_score(y_test, y_predicted_class_probabilities_logistic_regression, k=number_of_top_k), 3)))
-
-
-# print("\n# BASELINE MODELS")
 print("Random Model Test Accuracy:               " + str(round(top_k_accuracy_score(y_test, y_predicted_class_probabilities_random, k=number_of_top_k), 3)))
 print("Most Frequent Model Test Accuracy:        " + str(round(top_k_accuracy_score(y_test, y_predicted_class_probabilities_most_frequent, k=number_of_top_k), 3)))
 
