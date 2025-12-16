b32e47194bd17b19037ba4c4c43f3921e8df754f
brendanmcc02 <brendan.mccann02@gmail.com>
2025-12-16 17:00:59 +0000

    INSANE FIRST RESULTS LFG :sob: :fire: :ship: ga .ga .ga .ga .ga .

--- model-prototypes/traditional-prototype/traditional-prototype.py
+++ model-prototypes/traditional-prototype/traditional-prototype.py
@@ -11,17 +11,19 @@ user_riasec_vector = [ 0, 0, 0, 0, 0, 0, 0, 0, # R
                        4, 4, 1, 2, 2, 3, 2, 1, # S
                        0, 1, 3, 0, 2, 1, 0, 1, # E
                        1, 1, 4, 3, 4, 2, 2, 2] # C
-user_lc_subject_preferences = { "Mathematics": 4, 
+user_lc_subject_preferences = { 
+                                "Mathematics": 4, 
                                 "English": 3, 
                                 "Irish": 1, 
                                 "Physics": 4, 
                                 "Design and Communication Graphics": 4, 
                                 "Computer Science": 4, 
-                                "German": 2}
+                                "German": 2
+                                }
 
 filtered_cao_courses = get_filtered_cao_courses(user_college_course_preferences)
 
-top_k_recommendations = get_top_k_recommendations(filtered_cao_courses, user_riasec_vector, user_college_course_preferences, user_lc_subject_preferences, k=20, should_retrain_model=True)
+top_k_recommendations = get_top_k_recommendations(filtered_cao_courses, user_riasec_vector, user_college_course_preferences, user_lc_subject_preferences, k=20, should_retrain_model=False)
 
 for rec in top_k_recommendations:
     print(rec["title"] + "\n" + rec["college"] + "\nPoints: " + str(rec["points"]) + "\nSimilarity: " + str(round(rec["similarity"]*100.0, 1)) + "%\n")
