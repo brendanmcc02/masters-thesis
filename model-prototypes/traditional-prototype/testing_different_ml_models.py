01c6f3d6824b344d6dc45df186dce27aeaebe987
brendanmcc02 <brendan.mccann02@gmail.com>
2025-12-16 13:58:22 +0000

    deleting evaluation notes - LOOK HERE IF INTERESTED

--- model-prototypes/traditional-prototype/testing_different_ml_models.py
+++ model-prototypes/traditional-prototype/testing_different_ml_models.py
@@ -110,268 +110,27 @@ print_top_1_accuracy(y_test, "Hist Gradient Boosting Machine", "test", y_predict
 print_top_1_accuracy(y_train, "Hist Gradient Boosting Machine", "train", y_predicted_train_hist_gradient_boosting_machine)
 print_top_1_accuracy(y_test, "Most Frequent", "test", y_predicted_most_frequent)
 
-
-# # TRAINED MODELS, TOP K ACCURACY (K=3), EDUCATION_FILTER >= 2
-# Multinomial Naive Bayes Test Accuracy:    0.562
-# Categorical Naive Bayes Test Accuracy:    0.551
-# Logistic Regression Test Accuracy:        0.639 # fast
-# Logistic Regression Train Accuracy:       0.645 # no indication of significant over-fitting
-# SVM Test Accuracy:                        0.636 # so long
-# Gradient Boosting Machines Test Accuracy: 0.636 # fast
-# Random Forest Test Accuracy:              0.618
-# MLP Test Accuracy:                        0.622
-
-# # BASELINE MODELS
-# Random Model Test Accuracy:               0.199
-# Most Frequent Model Test Accuracy:        0.349
-
-
+# at least with Log Reg model, it seems to perform better with 0-4 likert
 # 0-2 LIKERT SCALE
 # Logistic Regression Test Accuracy:        0.629
 # Logistic Regression Test Accuracy:        0.633
 
-# # TOP K ACCURACY (K=1)
-# Logistic Regression Test Accuracy:        0.363
-# Random Model Test Accuracy:               0.065
-# Most Frequent Model Test Accuracy:        0.229
-
-# # TOP K ACCURACY (K=2)
-# Logistic Regression Test Accuracy:        0.53
-# Random Model Test Accuracy:               0.131
-# Most Frequent Model Test Accuracy:        0.289
-
-# # TOP K ACCURACY (K=4)
-# Logistic Regression Test Accuracy:        0.723
-# Random Model Test Accuracy:               0.265
-# Most Frequent Model Test Accuracy:        0.382
-
-# # TOP K ACCURACY (K=5)
-# Logistic Regression Test Accuracy:        0.788
-# Random Model Test Accuracy:               0.332
-# Most Frequent Model Test Accuracy:        0.385
-
-
-# CATEGORY                              PREDICTED    ACTUAL
-# agriculture & natural resources       0.0%         0.4%
-# arts                                  2.6%         4.6%
-# biology & life science                2.9%         4.4%
-# business                              25.7%        16.8%
-# communications & journalism           0.1%         3.2%
-# computers & mathematics               1.0%         3.7%
-# education                             0.1%         3.4%
-# engineering                           8.3%         7.8%
-# health                                3.0%         5.2%
-# humanities & liberal arts             11.2%        12.0%
-# industrial arts & consumer services   0.0%         0.3%
-# law & public policy                   0.0%         3.2%
-# physical sciences                     1.8%         6.1%
-# psychology & social work              43.2%        22.9%
-# social science                        0.2%         5.9%
-
+# at least with Log Reg model, it seems to perform better with no education filter
 # NO EDUCATION FILTER
 # Logistic Regression Top-3 Test Accuracy:        0.645
 # Logistic Regression Top-1 Test Accuracy:        0.37
 
+# at least with Log Reg model, it seems to perform better with no substring match
 # no substring match
 # Logistic Regression Top-3 Test Accuracy:        0.66
 # Logistic Regression Top-1 Test Accuracy:        0.379
 
 
-# Logistic Regression, class_weight = 'balanced'
-# # PROPORTIONS
-# CATEGORY    PREDICTED    ACTUAL
-# agriculture & natural resources 4.5%     0.6%
-# arts 7.3%     3.9%
-# biology & life science 7.0%     5.6%
-# business 9.7%     16.4%
-# communications 6.4%     2.7%
-# computers & mathematics 7.3%     4.8%
-# education 8.4%     4.3%
-# engineering 8.2%     9.0%
-# health 8.7%     7.7%
-# humanities & liberal arts 3.9%     9.9%
-# industrial arts & consumer services 5.9%     0.8%
-# law & public policy 3.5%     3.3%
-# physical sciences 5.8%     2.4%
-# psychology & social work 10.3%     22.8%
-# social science 3.1%     5.7%
-# Sum of Squared Differences: 348.63
-
-
-# # EVALUATION
-# Logistic Regression Top-3 Test Accuracy:        0.536
-# Most Frequent Model Top-3 Test Accuracy:        0.31
-# Logistic Regression Top-1 Test Accuracy:        0.265
-# Most Frequent Model Top-1 Test Accuracy:        0.228
-
-# HGBM, class_weight='balanced'
-# # PROPORTIONS
-# CATEGORY    PREDICTED    ACTUAL
-# agriculture & natural resources 1.9%     0.6%
-# arts 8.2%     3.9%
-# biology & life science 7.9%     5.6%
-# business 12.1%     16.4%
-# communications 6.0%     2.7%
-# computers & mathematics 7.2%     4.8%
-# education 8.0%     4.3%
-# engineering 8.7%     9.0%
-# health 8.7%     7.7%
-# humanities & liberal arts 5.0%     9.9%
-# industrial arts & consumer services 2.3%     0.8%
-# law & public policy 3.2%     3.3%
-# physical sciences 5.1%     2.4%
-# psychology & social work 12.0%     22.8%
-# social science 3.8%     5.7%
-# Sum of Squared Differences: 229.2
-
-
-# # EVALUATION
-# Gradient Boosting Machines Top-3 Test Accuracy: 0.56
-# Most Frequent Model Top-3 Test Accuracy:         0.31
-# Gradient Boosting Machines Top-1 Test Accuracy:        0.285
-# Most Frequent Model Top-1 Test Accuracy:        0.228
-
-# random forest, class_weight='balanced'/'balan200ced_subsample' (same results basically)
-# # PROPORTIONS
-# CATEGORY    PREDICTED    ACTUAL
-# agriculture & natural resources 0.1%     0.6%
-# arts 1.6%     3.9%
-# biology & life science 3.4%     5.6%
-# business 25.6%     16.4%
-# communications 0.0%     2.7%
-# computers & mathematics 0.9%     4.8%
-# education 0.2%     4.3%
-# engineering 9.5%     9.0%
-# health 4.4%     7.7%
-# humanities & liberal arts 6.0%     9.9%
-# industrial arts & consumer services 0.0%     0.8%
-# law & public policy 0.1%     3.3%
-# physical sciences 0.2%     2.4%
-# psychology & social work 47.8%     22.8%
-# social science 0.1%     5.7%
-
-# # EVALUATION
-# Random Forest Top-3 Test Accuracy: 0.636
-# Most Frequent Model Top-3 Test Accuracy:         0.31
-# Random Forest Top-1 Test Accuracy:        0.357
-# Most Frequent Model Top-1 Test Accuracy:        0.228
-
-
-## balanced random forest
-# # PROPORTIONS
-# CATEGORY    PREDICTED    ACTUAL
-# agriculture & natural resources 2.1%     0.6%
-# arts 8.8%     3.9%
-# biology & life science 8.7%     5.6%
-# business 12.3%     16.4%
-# communications 6.8%     2.7%
-# computers & mathematics 6.7%     4.8%
-# education 6.5%     4.3%
-# engineering 9.3%     9.0%
-# health 10.0%     7.7%
-# humanities & liberal arts 4.3%     9.9%
-# industrial arts & consumer services 1.9%     0.8%
-# law & public policy 3.2%     3.3%
-# physical sciences 6.1%     2.4%
-# psychology & social work 10.3%     22.8%
-# social science 3.0%     5.7%
-# Sum of Squared Differences: 293.13000000000005
-
-# # EVALUATION
-# Gradient Boosting Machines Top-3 Test Accuracy: 0.56
-# Balanced Random Forest Top-3 Test Accuracy: 0.523
-# Gradient Boosting Machines Top-1 Test Accuracy:        0.285
-# Balanced Random Forest Top-1 Test Accuracy:        0.264
-
-
-
-# BALANCED RANDOM FOREST
-# # PROPORTIONS - TEST
-# CATEGORY    PREDICTED    ACTUAL
-# agriculture & natural resources 2.1%     0.6%
-# arts 8.8%     3.9%
-# biology & life science 8.7%     5.6%
-# business 12.3%     16.4%
-# communications 6.8%     2.7%
-# computers & mathematics 6.7%     4.8%
-# education 6.5%     4.3%
-# engineering 9.3%     9.0%
-# health 10.0%     7.7%
-# humanities & liberal arts 4.3%     9.9%
-# industrial arts & consumer services 1.9%     0.8%
-# law & public policy 3.2%     3.3%
-# physical sciences 6.1%     2.4%
-# psychology & social work 10.3%     22.8%
-# social science 3.0%     5.7%
-
-# Sum of Squared Differences: 293.13
-
-# # PROPORTIONS - TRAIN
-# CATEGORY    PREDICTED    ACTUAL
-# agriculture & natural resources 1.7%     0.6%
-# arts 8.7%     4.0%
-# biology & life science 8.4%     5.6%
-# business 12.0%     16.4%
-# communications 6.6%     2.7%
-# computers & mathematics 6.8%     4.8%
-# education 7.0%     4.3%
-# engineering 9.0%     9.0%
-# health 9.7%     7.7%
-# humanities & liberal arts 4.6%     9.9%
-# industrial arts & consumer services 1.9%     0.8%
-# law & public policy 4.1%     3.3%
-# physical sciences 5.8%     2.4%
-# psychology & social work 9.6%     22.8%
-# social science 4.1%     5.7%
-
-# Sum of Squared Differences: 299.3
-
-# # EVALUATION
-# 0.523 - Balanced Random Forest Top-3 test Accuracy
-# 0.791 - Balanced Random Forest Top-3 train Accuracy
-# 0.264 - Balanced Random Forest Top-1 test Accuracy
-# 0.519 - Balanced Random Forest Top-1 train Accuracy
-
-
 # HGBM
 # # PROPORTIONS - TEST
-# CATEGORY    PREDICTED    ACTUAL
-# agriculture & natural resources 1.9%     0.6%
-# arts 8.2%     3.9%
-# biology & life science 7.9%     5.6%
-# business 12.1%     16.4%
-# communications 6.0%     2.7%
-# computers & mathematics 7.2%     4.8%
-# education 8.0%     4.3%
-# engineering 8.7%     9.0%
-# health 8.7%     7.7%
-# humanities & liberal arts 5.0%     9.9%
-# industrial arts & consumer services 2.3%     0.8%
-# law & public policy 3.2%     3.3%
-# physical sciences 5.1%     2.4%
-# psychology & social work 12.0%     22.8%
-# social science 3.8%     5.7%
-
 # Sum of Squared Differences: 229.2
 
 # # PROPORTIONS - TRAIN
-# CATEGORY    PREDICTED    ACTUAL
-# agriculture & natural resources 1.9%     0.6%
-# arts 8.3%     4.0%
-# biology & life science 7.4%     5.6%
-# business 11.6%     16.4%
-# communications 6.3%     2.7%
-# computers & mathematics 6.7%     4.8%
-# education 7.4%     4.3%
-# engineering 8.6%     9.0%
-# health 9.3%     7.7%
-# humanities & liberal arts 5.3%     9.9%
-# industrial arts & consumer services 2.6%     0.8%
-# law & public policy 3.6%     3.3%
-# physical sciences 5.1%     2.4%
-# psychology & social work 11.8%     22.8%
-# social science 4.0%     5.7%
-
 # Sum of Squared Differences: 231.03
 
 # # EVALUATION
@@ -383,44 +142,10 @@ print_top_1_accuracy(y_test, "Most Frequent", "test", y_predicted_most_frequent)
 
 # LOGISTIC REGRESSION
 # # PROPORTIONS - TEST
-# CATEGORY    PREDICTED    ACTUAL
-# agriculture & natural resources 4.5%     0.6%
-# arts 7.3%     3.9%
-# biology & life science 7.0%     5.6%
-# business 9.7%     16.4%
-# communications 6.4%     2.7%
-# computers & mathematics 7.3%     4.8%
-# education 8.4%     4.3%
-# engineering 8.2%     9.0%
-# health 8.7%     7.7%
-# humanities & liberal arts 3.9%     9.9%
-# industrial arts & consumer services 5.9%     0.8%
-# law & public policy 3.5%     3.3%
-# physical sciences 5.8%     2.4%
-# psychology & social work 10.3%     22.8%
-# social science 3.1%     5.7%
-
 # Sum of Squared Differences: 348.63
 
 # # PROPORTIONS - TRAIN
-# CATEGORY    PREDICTED    ACTUAL
-# agriculture & natural resources 4.7%     0.6%
-# arts 7.7%     4.0%
-# biology & life science 6.3%     5.6%
-# business 10.1%     16.4%
-# communications 6.4%     2.7%
-# computers & mathematics 6.7%     4.8%
-# education 7.9%     4.3%
-# engineering 7.9%     9.0%
-# health 9.5%     7.7%
-# humanities & liberal arts 4.0%     9.9%
-# industrial arts & consumer services 5.7%     0.8%
-# law & public policy 3.7%     3.3%
-# physical sciences 6.1%     2.4%
-# psychology & social work 10.3%     22.8%
-# social science 3.1%     5.7%
-
-# Sum of Squared Differences: 341.07
+# # Sum of Squared Differences: 341.07
 
 # # EVALUATION
 # 0.536 - Logistic Regression Top-3 test Accuracy
