6fe0b75c505f8ae21aa24c6c1178b893142fbdf0
brendanmcc02 <brendan.mccann02@gmail.com>
2025-12-17 23:50:53 +0000

    aysha's stuff

--- model-prototypes/traditional-prototype/traditional-prototype.py
+++ model-prototypes/traditional-prototype/traditional-prototype.py
@@ -4,21 +4,21 @@ user_college_course_preferences = { "nfq_levels": [8],
                                     "colleges": ["Trinity College Dublin - TCD", 
                                                  "University College Dublin - UCD", 
                                                  "RCSI University of Medicine and Health Sciences"], 
-                                    "expected_points": 589}
-user_riasec_vector = [ 0, 0, 0, 0, 0, 0, 0, 0, # R
-                       3, 3, 4, 4, 4, 3, 3, 2, # I
-                       0, 2, 0, 0, 2, 0, 0, 0, # A
-                       4, 4, 1, 2, 2, 3, 2, 1, # S
-                       0, 1, 3, 0, 2, 1, 0, 1, # E
-                       1, 1, 4, 3, 4, 2, 2, 2] # C
+                                    "expected_points": 732}
+user_riasec_vector = [ 2, 0, 0, 2, 0, 0, 1, 0, # R
+                       4, 3, 3, 4, 3, 3, 2, 2, # I
+                       0, 2, 3, 1, 3, 2, 0, 3, # A
+                       4, 4, 4, 2, 4, 3, 4, 4, # S
+                       0, 0, 2, 0, 2, 1, 1, 1, # E
+                       2, 0, 1, 2, 2, 3, 2, 2] # C
 user_lc_subject_preferences = { 
-                                "Mathematics": 4, 
-                                "English": 3, 
-                                "Irish": 1, 
-                                "Physics": 4, 
-                                "Design and Communication Graphics": 4, 
-                                "Computer Science": 4, 
-                                "German": 2
+                                "Mathematics": 3, 
+                                "English": 4, 
+                                "Irish": 4, 
+                                "Business": 2, 
+                                "Chemistry": 4, 
+                                "Biology": 3, 
+                                "German": 4
                                 }
 
 filtered_cao_courses = get_filtered_cao_courses(user_college_course_preferences)
