ba95ed524454c3f5ec18bfe47ff73f27a6f06aa1
mccannbr <mccannbr@tcd.ie>
2026-01-13 14:29:52 +0000

    idk bro

--- careers-portal-web-scraper/sbox.py
+++ careers-portal-web-scraper/sbox.py
@@ -1,41 +1,28 @@
+
+
+#```get riasec makeup of each category - could apply this as a mask to the user's riasec profile?
 import json
 
 file = open("../datasets/cao-college-courses/cao-college-courses.json")
 courses = json.load(file)
 
+categories_dict = {}
+r_i = ["realistic", "investigative", "artistic", "social", "enterprising", "conventional"]
+
 for course in courses:
     title = course["title"].lower()
-    
-    if "healthcare" in course["categories"] and "investigative" in course["interests"] and "social" not in course["interests"]:
-        print(title)
+    for cat in course["categories"]:
+        if cat not in categories_dict:
+            categories_dict[cat] = [0] * 6
 
-# ```get riasec makeup of each category - could apply this as a mask to the user's riasec profile?
-# import json
-
-# file = open("../datasets/cao-college-courses/cao-college-courses.json")
-# courses = json.load(file)
-
-# categories_dict = {}
-# r_i = ["realistic", "investigative", "artistic", "social", "enterprising", "conventional"]
-
-# for course in courses:
-#     title = course["title"].lower()
-#     for cat in course["categories"]:
-#         if cat not in categories_dict:
-#             categories_dict[cat] = [0] * 6
-
-#         for inte in course["interests"]:
-#             categories_dict[cat][r_i.index(inte)] += 1
-
-#     if "creative arts" in course["categories"] and "conventional" in course["interests"]:
-#         print(title)
+        for inte in course["interests"]:
+            categories_dict[cat][r_i.index(inte)] += 1
 
 
-# print("\n\n")
-# for k, v in categories_dict.items():
-#     norm = [round(float(i)/max(v), 2) for i in v]
+for k, v in categories_dict.items():
+    norm = [round(float(i)/max(v), 2) for i in v]
 
-#     print(k + ": " + str(norm))
+    print(k + ": " + str(norm))
 
-# # with open("../datasets/cao-college-courses/cao-college-courses.json", "w") as outfile:
-# #     json.dump(courses, outfile, indent=4)
+# with open("../datasets/cao-college-courses/cao-college-courses.json", "w") as outfile:
+#     json.dump(courses, outfile, indent=4)
