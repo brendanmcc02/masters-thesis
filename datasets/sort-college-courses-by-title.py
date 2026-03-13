9ec256c10617cb75eb9ca804f36f93cf67fc6b8d
brendanmcc02 <brendan.mccann02@gmail.com>
2026-03-13 13:18:32 +0000

    meeting notes

--- datasets/sort-college-courses-by-title.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open('cao-college-courses.json', 'r') as f:
-    courses = json.load(f)
-
-courses.sort(key=lambda x: x['title'].lower())
-
-with open('cao-college-courses.json', 'w') as f:
-    json.dump(courses, f, indent=4)
